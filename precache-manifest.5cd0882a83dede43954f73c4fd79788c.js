844831fda00c4b5fb980d282c2a0c757494e5ef1
SHREYA MISHRA <61110378+shreya-mishra@users.noreply.github.com>
2020-05-15 21:14:49 +0530

    Updates

---
+++ precache-manifest.5cd0882a83dede43954f73c4fd79788c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d197cd8aec698b58550ca1ef1c8e8f9",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "b0b323ee83810c9e2fe4",
+    "url": "/Monsters-Rolodex/static/css/main.bcebc593.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/Monsters-Rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0b323ee83810c9e2fe4",
+    "url": "/Monsters-Rolodex/static/js/main.f0e5ee6e.chunk.js"
+  },
+  {
+    "revision": "bb1db29ffdd7a4ded20c",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.f852ebba.js"
+  }
+]);
